e9d506a243b393b85ef3095e01a9aa8d979c0cf6
Alfinete <leandrocarra@gmail.com>
2021-07-11 19:07:50 -0300

    09072021" "

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import './assets/styles/reset.css';
+import './assets/styles/pattern.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import Sexta09 from './Pages/09072021';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/09072021" component={Sexta09} />
+        <Route path="/*" component={App} />
+      </Switch>
+    </ BrowserRouter>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
